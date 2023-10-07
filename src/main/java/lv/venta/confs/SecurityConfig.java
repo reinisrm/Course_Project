adacbc12bd270e3d45e1d5fa16922799545b57a1
reinisrm <rreeiinniiss@gmail.com>
2023-10-07 20:15:42 +0300

    Updated SecurityConfig

--- src/main/java/lv/venta/confs/SecurityConfig.java
+++ src/main/java/lv/venta/confs/SecurityConfig.java
@@ -2,9 +2,17 @@ package lv.venta.confs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import lv.venta.services.impl.security.MyUserDetailsManagerImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +23,31 @@ public class SecurityConfig {
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**")); 
 	}
+	@Bean
+	public MyUserDetailsManagerImpl userDetailsManeger() {
+		MyUserDetailsManagerImpl manager = new MyUserDetailsManagerImpl();
+		return manager;
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoderSimple2() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		
+		
+		authenticationManagerBuilder.
+		userDetailsService(userDetailsManeger()).passwordEncoder(passwordEncoderSimple2());
+		return authenticationManagerBuilder.build();
+	}
+
 	
+
+
 	
 	
 	
