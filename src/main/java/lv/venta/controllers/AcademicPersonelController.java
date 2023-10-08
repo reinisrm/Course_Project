113a6a0fb8b482321c86d4cac874c15829be22bd
reinisrm <rreeiinniiss@gmail.com>
2023-10-08 19:33:56 +0300

    Added: log4j2 and slf4j dependencies, log4j2.xml, added Spring Boot Logging for AcademicPersonelController

--- src/main/java/lv/venta/controllers/AcademicPersonelController.java
+++ src/main/java/lv/venta/controllers/AcademicPersonelController.java
@@ -2,6 +2,8 @@ package lv.venta.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +13,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import lv.venta.enums.Degree;
 import lv.venta.models.users.Academic_personel;
 import lv.venta.models.users.User;
 import lv.venta.services.users.impl.AcademicPersonelCRUDService;
 import lv.venta.services.users.impl.UserCRUDService;
 
+@Slf4j
 @Controller
 public class AcademicPersonelController {
+	
+    public void log() {
+        log.debug("Debug message");
+        log.info("Informational message");
+        log.warn("Warning message");
+        log.error("Error message");
+    }
+    
+    public void callLogMethod() {
+        log();
+    }
 	
 	@Autowired
 	AcademicPersonelCRUDService personelService;
