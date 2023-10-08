48179b045c51e42aa73b561cc4446935577f1cbb
reinisrm <rreeiinniiss@gmail.com>
2023-10-08 21:46:09 +0300

    Updated securityConfig

--- src/main/java/lv/venta/confs/SecurityConfig.java
+++ src/main/java/lv/venta/confs/SecurityConfig.java
@@ -44,6 +44,44 @@ public class SecurityConfig {
 		userDetailsService(userDetailsManeger()).passwordEncoder(passwordEncoderSimple2());
 		return authenticationManagerBuilder.build();
 	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+
+			.requestMatchers("/personel/showAll").permitAll()
+			.requestMatchers("/personel/showOne/**").permitAll()
+			.requestMatchers("/personel/delete/**").permitAll()
+			.requestMatchers("/personel/add").permitAll()
+			.requestMatchers("/personel/update/**").permitAll()
+			.requestMatchers("/comments/showAll").permitAll()
+			.requestMatchers("/comments/showOne/**").permitAll()
+			.requestMatchers("/comments/add").permitAll()
+			//.requestMatchers("/comments/delete").permitAll()
+			.requestMatchers("/courses/showAll").permitAll()
+			.requestMatchers("/courses/showOne/**").permitAll()
+			.requestMatchers("/courses/add").permitAll()
+			.requestMatchers("/courses/addDebt/**/**").permitAll()
+			.requestMatchers("/courses/removeDebt/**/**").permitAll()
+			.requestMatchers("/login").permitAll()
+			.requestMatchers("/register").permitAll()
+			.requestMatchers("/student/showAll").permitAll()
+			.requestMatchers("/student/show/**").permitAll()
+			.requestMatchers("/student/remove/**").permitAll()
+			.requestMatchers("/student/insertNew").permitAll()
+			.requestMatchers("/update/**").permitAll()
+			.requestMatchers("/thesis/showAll").permitAll()
+			.requestMatchers("/thesis/show/**").permitAll()
+			.requestMatchers("/thesis/remove/**").permitAll()
+			.requestMatchers("/thesis/insertNew").permitAll()
+			.requestMatchers("/thesis/update/**").permitAll()
+			.and()
+			.formLogin().permitAll()
+			.and()
+			.logout().permitAll();
+
+		return http.build();
+	}
 
 	
 
