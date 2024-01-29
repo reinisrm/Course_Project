a81056911bbaf3e4c7a72d65d26d95a9f0ac3999
reinisrm <rreeiinniiss@gmail.com>
2024-01-29 19:38:17 +0200

    Fixed Spring security to up-to-date standarts with lambda

--- src/main/java/lv/venta/confs/SecurityConfig.java
+++ src/main/java/lv/venta/confs/SecurityConfig.java
@@ -3,8 +3,6 @@ package lv.venta.confs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,14 +17,6 @@ import lv.venta.services.impl.security.MyUserDetailsManagerImpl;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	/*
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**")); 
-	}
-	*/
-	
 
 	//@Bean
 	public MyUserDetailsManagerImpl userDetailsManeger() {
@@ -52,43 +42,39 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-			.requestMatchers("/home").permitAll()
-			.requestMatchers("/personel/showAll").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/personel/showOne/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/personel/delete/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/personel/add").hasAnyAuthority("ADMIN")
-			.requestMatchers("/personel/update/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/comments/showAll").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/comments/showOne/**").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/comments/add").hasAnyAuthority("ADMIN")
-			.requestMatchers("/comments/delete").hasAnyAuthority("ADMIN")
-			.requestMatchers("/courses/showAll").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/courses/showOne/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/courses/add").hasAnyAuthority("ADMIN")
-			.requestMatchers("/courses/addDebt/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/courses/removeDebt/**").hasAnyAuthority("ADMIN")
-			//.requestMatchers("/login").permitAll()
-			//.requestMatchers("/register").permitAll()
-			.requestMatchers("/student/showAll").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/student/show/**").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/student/remove/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/student/insertNew").hasAnyAuthority("ADMIN")
-			.requestMatchers("/student/update/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/thesis/showAll").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/thesis/show/**").hasAnyAuthority("ADMIN", "USER")
-			.requestMatchers("/thesis/remove/**").hasAnyAuthority("ADMIN")
-			.requestMatchers("/thesis/insertNew").hasAnyAuthority("ADMIN")
-			.requestMatchers("/thesis/update/**").hasAnyAuthority("ADMIN")
-	        .and()
-	        .formLogin()
-	        //.loginPage("/login")  // Specify the custom login page
-	        .defaultSuccessUrl("/home")  // Redirect after successful login
-	        .permitAll()
-	        .and()
-	        .logout()
-	        .permitAll(); 
-		
+		http.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/home").permitAll()
+						.requestMatchers("/personel/showAll").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/personel/showOne/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/personel/delete/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/personel/add").hasAnyAuthority("ADMIN")
+						.requestMatchers("/personel/update/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/comments/showAll").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/comments/showOne/**").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/comments/add").hasAnyAuthority("ADMIN")
+						.requestMatchers("/comments/delete").hasAnyAuthority("ADMIN")
+						.requestMatchers("/courses/showAll").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/courses/showOne/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/courses/add").hasAnyAuthority("ADMIN")
+						.requestMatchers("/courses/addDebt/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/courses/removeDebt/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/student/showAll").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/student/show/**").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/student/remove/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/student/insertNew").hasAnyAuthority("ADMIN")
+						.requestMatchers("/student/update/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/thesis/showAll").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/thesis/show/**").hasAnyAuthority("ADMIN", "USER")
+						.requestMatchers("/thesis/remove/**").hasAnyAuthority("ADMIN")
+						.requestMatchers("/thesis/insertNew").hasAnyAuthority("ADMIN")
+						.requestMatchers("/thesis/update/**").hasAnyAuthority("ADMIN"))
+						.formLogin(login -> login
+								.defaultSuccessUrl("/home")  // Redirect after successful login
+								.permitAll())
+						.logout(logout -> logout
+								.permitAll());
+
+
 
 
 		 
